a8aa2b34f2aea79b0029cbf798de10e18e9a75d5
Danila Dippel <erroraeto@gmail.com>
2025-11-02 17:47:49 +0300

    typescript add

--- src/script.tsx
+++ src/script.tsx
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom/client';
 import { App } from './Components/App/App';
 import './common.css';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
